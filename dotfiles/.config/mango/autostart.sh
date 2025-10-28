eb12fd8f828de36464b55fb3199ca7574c61f608
shorinkiwata <fcl709@outlook.com>
2025-10-28 19:14:06 +0900

    update

--- dotfiles/.config/mango/autostart.sh
+++ dotfiles/.config/mango/autostart.sh
@@ -13,7 +13,4 @@ swww-daemon &
 
 fcitx5 & copyq & 
 
-dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP=wlroots
-# The next line of command is not necessary. It is only to avoid some situations where it cannot start automatically
-/usr/lib/xdg-desktop-portal-wlr &
 
