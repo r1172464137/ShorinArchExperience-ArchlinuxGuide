80edf66fe104751a60ff6e4346bec9b3dbe61351
shorinkiwata <fcl709@outlook.com>
2025-11-22 21:31:04 +0900

    update

--- dotfiles/.config/niri/scripts/swayidle.sh
+++ dotfiles/.config/niri/scripts/swayidle.sh
@@ -5,4 +5,4 @@ exec swayidle -w \
 timeout 300  'swaylock -f' \
 timeout 600  'niri msg action power-off-monitors' \
 resume       'niri msg action power-on-monitors' \
-timeout 1200 'sytstemctl suspend'
+timeout 1200 'systemctl suspend'
