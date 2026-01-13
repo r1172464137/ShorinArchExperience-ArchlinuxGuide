9fd61e8498ffe7ffc6731c0d265d317640a1f82e
shorinkiwata <fcl709@outlook.com>
2026-01-13 17:18:06 +0900

    dotfiles cava cpu usage fix

--- dotfiles/.config/waybar-niri-Win11Like/scripts/cava.sh
+++ dotfiles/.config/waybar-niri-Win11Like/scripts/cava.sh
@@ -1,9 +1,30 @@
-#! /bin/bash
+#!/bin/bash
 
 bar="▁▂▃▄▅▆▇█"
-dict="s/;//g;"
+config_file="/tmp/waybar_cava_config"
 
-# creating "dictionary" to replace char with bar
+# 当脚本退出(EXIT)或被中断时，自动杀掉当前脚本的子进程(cava)
+trap "pkill -P $$" EXIT
+
+# --- 2. 生成配置 ---
+echo "
+[general]
+framerate = 24
+sleep_timer = 1   
+bars = 14
+
+[input]
+method = pulse
+
+[output]
+method = raw
+raw_target = /dev/stdout
+data_format = ascii
+ascii_max_range = 7
+" > "$config_file"
+
+# --- 3. 构建字典 ---
+dict="s/;//g;"
 i=0
 while [ $i -lt ${#bar} ]
 do
@@ -11,20 +32,5 @@ do
     i=$((i=i+1))
 done
 
-# write cava config
-config_file="/tmp/polybar_cava_config"
-echo "
-[general]
-bars = 14
-
-[output]
-method = raw
-raw_target = /dev/stdout
-data_format = ascii
-ascii_max_range = 7
-" > $config_file
-
-# read stdout from cava
-cava -p $config_file | while read -r line; do
-    echo $line | sed $dict
-done
+# --- 4. 运行 ---
+cava -p "$config_file" | sed -u "$dict"
