cd5a969be8cf4c9a4f2ea0f99d75d3d7ea4bc858
shorinkiwata <fcl709@outlook.com>
2026-02-15 20:25:54 +0900

    [DOT] 移动hypr配置到legacy目录。移动hyprlock.conf到niri目录，修改matugen生成的hyprlock颜色文件到缓存目录。

--- dotfiles/.config/niri/scripts/swayidle.sh
+++ dotfiles/.config/niri/scripts/swayidle.sh
@@ -2,7 +2,7 @@
 
 # 5分钟锁屏，10分钟熄屏，20分钟休眠
 exec swayidle -w \
-timeout 600  'hyprlock &' \
+timeout 600  'hyprlock -c ~/.config/niri/hyprlock.conf &' \
 timeout 900  'niri msg action power-off-monitors' \
 resume       'niri msg action power-on-monitors' \
 timeout 1800 'systemctl suspend'
